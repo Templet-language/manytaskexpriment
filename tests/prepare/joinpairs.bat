36d903001734276cc5331f51c5e6d0d9810585ff
SergeyVostokin <easts@mail.ru>
2019-11-09 22:01:13 +0400

    data set utils are fixed

--- tests/prepare/joinpairs.bat
+++ tests/prepare/joinpairs.bat
@@ -7,27 +7,27 @@ SET par=EVEN
 SET fname=
 SET fnamenext=
 
-FOR /F "tokens=*" %%G IN ('dir /b D:\GitHub\manytaskexpriment\tests\files\*') DO (
+FOR /F "tokens=*" %%G IN ('dir /b E:\*') DO (
     SET fname=file
     SET fnamenext=file
     SET /A next = count + 1	
 	
-	IF !count! LSS 10000 SET fname=!fname!0
-	IF !count! LSS 1000 SET fname=!fname!0
+	rem IF !count! LSS 10000 SET fname=!fname!0
+	rem IF !count! LSS 1000 SET fname=!fname!0
     IF !count! LSS 100 SET fname=!fname!0
 	IF !count! LSS 10 SET fname=!fname!0
  	
-	IF !next! LSS 10000 SET fnamenext=!fnamenext!0
-	IF !next! LSS 1000 SET fnamenext=!fnamenext!0
+	rem IF !next! LSS 10000 SET fnamenext=!fnamenext!0
+	rem IF !next! LSS 1000 SET fnamenext=!fnamenext!0
     IF !next! LSS 100 SET fnamenext=!fnamenext!0
 	IF !next! LSS 10 SET fnamenext=!fnamenext!0
 	
-	SET fname=!fname!!count!.txt
-	SET fnamenext=!fnamenext!!next!.txt
+	SET fname=!fname!!count!.v00
+	SET fnamenext=!fnamenext!!next!.v00
 	
-	IF /I !par!==EVEN COPY  .\files\!fname! + .\files\!fnamenext! .\files\!fname!.par
-    IF /I !par!==EVEN ERASE .\files\!fname! /Q
-    IF /I !par!==EVEN ERASE .\files\!fnamenext! /Q	
+	IF /I !par!==EVEN COPY  E:\!fname! + E:\!fnamenext! E:\!fname!.par
+    IF /I !par!==EVEN ERASE E:\!fname! /Q
+    IF /I !par!==EVEN ERASE E:\!fnamenext! /Q	
 	
 	SET /A count = count + 1
 	
--- tests/prepare/myrename.bat
+++ tests/prepare/myrename.bat
@@ -4,15 +4,15 @@ SETLOCAL EnableDelayedExpansion
 SET count=0
 SET fname=
 
-FOR /F "tokens=*" %%G IN ('dir /b D:\GitHub\manytaskexpriment\tests\files\*') DO (
+FOR /F "tokens=*" %%G IN ('dir /b E:\*') DO (
 	SET fname=file
 	
-	IF !count! LSS 10000 SET fname=!fname!0
-	IF !count! LSS 1000 SET fname=!fname!0
+	rem IF !count! LSS 10000 SET fname=!fname!0
+	rem IF !count! LSS 1000 SET fname=!fname!0
     IF !count! LSS 100 SET fname=!fname!0
 	IF !count! LSS 10 SET fname=!fname!0
  	
-	REN .\files\"%%G" !fname!!count!.txt
+	REN "E:\%%G" !fname!!count!.v00
 	
 	SET /A count = count + 1 
 )
